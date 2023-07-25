f623d7cb4387ce191364a02a7b51e14017cf182a
BandiMannaus <140480040+BandiMannaus@users.noreply.github.com>
2023-07-25 10:52:34 +0530

    Add files via upload

---
+++ 1-notebook.png.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+# 1.Python
+# 2.R
+# 3.SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1.Pandas
+# 2.Scikit-learn
+# 3.Matplotlib
+
+# Data Science Tools
+# 
+# 1.Jupyter Notebook	 
+# 2.RStudio	
+# 3.Visual Studio Code
+
+# Below are a few examples of evaluating arithmetic expressions in Python
+# 
+# # Addition
+# result_add = 5 + 3
+# print(result_add)  # Output: 8
+# 
+# # Subtraction
+# result_sub = 10 - 4
+# print(result_sub)  # Output: 6
+# 
+# # Multiplication
+# result_mul = 6 * 7
+# print(result_mul)  # Output: 42
+# 
+# # Division
+# result_div = 15 / 3
+# print(result_div)  # Output: 5.0 (Note: In Python 3, division always returns a float)
+# 
+# # Integer Division (Floor Division)
+# result_int_div = 15 // 4
+# print(result_int_div)  # Output: 3 (Rounds down to the nearest whole number)
+
+# This a simple arithmetic expression to mutiply then add integers
+
+# In[1]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+# In[2]:
+
+
+minutes = 200
+hours = minutes / 60
+print(hours)
+
+
+# **Objectives:**
+# List popular languages for Data Science
+# Data Science Tools
+# Commonly used libraries used by Data Scientists
+# Popular languages that Data Scientists use
+#     
+
+# ## Author
+# Bandi Mannaus
